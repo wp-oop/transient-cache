42322c40142f8880ba28fb8830a0a4f667a50cec
Miguel Muscat <miguelmuscat93@gmail.com>
2020-03-30 18:01:59 +0200

    Added default TTL to factory

--- src/CachePoolFactory.php
+++ src/CachePoolFactory.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace WpOop\TransientCache;
 
+use DateInterval;
 use Psr\SimpleCache\CacheInterface;
 use wpdb;
 
@@ -15,10 +16,15 @@ class CachePoolFactory implements CachePoolFactoryInterface
      * @var wpdb
      */
     protected $wpdb;
+    /**
+     * @var int|DateInterval
+     */
+    protected $defaultTtl;
 
-    public function __construct(wpdb $wpdb)
+    public function __construct(wpdb $wpdb, $defaultTtl = 0)
     {
         $this->wpdb = $wpdb;
+        $this->defaultTtl = $defaultTtl;
     }
 
     /**
@@ -27,7 +33,7 @@ class CachePoolFactory implements CachePoolFactoryInterface
     public function createCachePool(string $poolName): CacheInterface
     {
         $default = uniqid('default');
-        $pool = new CachePool($this->wpdb, $poolName, $default);
+        $pool = new CachePool($this->wpdb, $poolName, $default, $this->defaultTtl);
 
         return $pool;
     }
